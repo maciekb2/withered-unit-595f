90b9fa9a01dc6ff5a594ecd3b5bc82201c8b1e58
mbrzezny <maciej.brzezny@salesmanago.com>
2026-07-13 03:02:39 +0200

    Keep carousel migration idempotent at ten slides

--- deploy/selfhosted/migrations/003_social_carousels.sql
+++ deploy/selfhosted/migrations/003_social_carousels.sql
@@ -1,6 +1,10 @@
 ALTER TABLE social_assets DROP CONSTRAINT IF EXISTS social_assets_kind_check;
 ALTER TABLE social_assets ADD CONSTRAINT social_assets_kind_check CHECK (
-  kind IN ('reel','instagram_post','carousel_01','carousel_02','carousel_03','carousel_04','carousel_05','carousel_06')
+  kind IN (
+    'reel','instagram_post',
+    'carousel_01','carousel_02','carousel_03','carousel_04','carousel_05',
+    'carousel_06','carousel_07','carousel_08','carousel_09','carousel_10'
+  )
 );
 
 ALTER TABLE social_publications DROP CONSTRAINT IF EXISTS social_publications_channel_check;
